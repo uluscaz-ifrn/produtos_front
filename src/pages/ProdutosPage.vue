431127d47f7c4c40f9e2e6b09eac2b7d72bb651e
uluscaz-ifrn <dantas.lucas2@escolar.ifrn.edu.br>
2025-05-02 11:32:53 -0300

    criar pagina de produtos

---
+++ src/pages/ProdutosPage.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import ListProduct from '../components/ListarProdutos.vue'
+import { excluirProduto } from '@/services/produtoService'
+import Form from '../components/Form.vue'
+import { cadastrarProduto } from '@/services/produtoService'
+async function cadastrar(produto: { nome: string; preco: number }) {
+  try {
+    await cadastrarProduto(produto)
+    alert('Produto cadastrado com sucesso!')
+  } catch (error) {
+    console.error('Erro ao cadastrar produto:', error)
+    alert('Erro ao cadastrar produto')
+  }
+}
+</script>
+<template>
+  <Form @submitProduto="cadastrar" />
+  <ListProduct @excluir="excluirProduto" />
+</template>
