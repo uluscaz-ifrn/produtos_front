8a0d3cca2f495def1b0a8b12bd1ea368b2b23292
uluscaz-ifrn <dantas.lucas2@escolar.ifrn.edu.br>
2025-05-02 12:54:39 -0300

    importar metodos e renomear componente

--- src/pages/ProdutosPage.vue
+++ src/pages/ProdutosPage.vue
@@ -1,19 +1,22 @@
 <script setup lang="ts">
-import ListProduct from '../components/ListarProdutos.vue'
-import { excluirProduto } from '@/services/produtoService'
+import ListarProdutos from '../components/ListarProdutos.vue'
 import Form from '../components/Form.vue'
-import { cadastrarProduto } from '@/services/produtoService'
+import { cadastrarProduto, produtos, carregarProdutos, excluirProduto } from '@/services/produtoService'
+import { onMounted } from 'vue'
+
 async function cadastrar(produto: { nome: string; preco: number }) {
   try {
     await cadastrarProduto(produto)
     alert('Produto cadastrado com sucesso!')
+   carregarProdutos()
   } catch (error) {
     console.error('Erro ao cadastrar produto:', error)
     alert('Erro ao cadastrar produto')
   }
 }
+onMounted(carregarProdutos)
 </script>
 <template>
   <Form @submitProduto="cadastrar" />
-  <ListProduct @excluir="excluirProduto" />
+  <ListarProdutos @excluir="excluirProduto" :Produtos="produtos"/>
 </template>
