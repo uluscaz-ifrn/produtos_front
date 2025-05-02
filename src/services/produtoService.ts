1b37b43780f5d0e2776d403cc9f973cf3d6829a7
uluscaz-ifrn <dantas.lucas2@escolar.ifrn.edu.br>
2025-05-02 12:33:31 -0300

    funcao de carregar de produtos para usar nos componentes

--- src/services/produtoService.ts
+++ src/services/produtoService.ts
@@ -1,8 +1,17 @@
 import axios from 'axios'
 const baseUrl = 'http://localhost:3000/produtos'
+import type { Produto } from '../types/index'
+import { ref } from 'vue'
+
+export const produtos= ref<Produto[]>([])
+
 export async function listarProdutos() {
   const resultado = await axios.get(baseUrl)
   const produtos = resultado.data
+  return produtos
+}
+export async function carregarProdutos(){
+  produtos.value = await listarProdutos()
   return produtos
 }
 export async function cadastrarProduto(produto: { nome: string; preco: number }) {
@@ -10,6 +19,7 @@ export async function cadastrarProduto(produto: { nome: string; preco: number })
   const produtoCadastrado = resultado.data
   return produtoCadastrado
 }
+
 export async function excluirProduto(id: number) {
   const resultado = await axios.delete(`${baseUrl}/${id}`)
   return resultado.data
